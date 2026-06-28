edf98fcf2dc9f423495d79e4c414e1702a651bf3
Семён Бабенков <semen@MacBook-Pro-Semen.local>
2026-06-28 23:03:09 +0300

    Simplify Windows batch launcher

--- launchers/windows/start-converter.bat
+++ launchers/windows/start-converter.bat
@@ -2,26 +2,13 @@
 setlocal
 
 set "APP_DIR=%~dp0..\.."
-for %%I in ("%APP_DIR%") do set "APP_DIR=%%~fI"
 set "LAUNCHER=%APP_DIR%\launchers\start_converter.py"
 
-if not exist "%LAUNCHER%" (
-    echo Launcher was not found:
-    echo %LAUNCHER%
-    goto done
-)
+py -3 "%LAUNCHER%"
+if not errorlevel 9009 goto done
 
-where py >nul 2>nul
-if not errorlevel 1 (
-    py -3 "%LAUNCHER%"
-    goto done
-)
-
-where python >nul 2>nul
-if not errorlevel 1 (
-    python "%LAUNCHER%"
-    goto done
-)
+python "%LAUNCHER%"
+if not errorlevel 9009 goto done
 
 echo Python 3 was not found.
 echo Install Python 3.10 or newer and run this file again.
